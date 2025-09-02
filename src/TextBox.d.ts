a75c8ecb117981db026f9fb59765a4d0a8b65897
sogo <241095079@ccmailg.meijo-u.ac.jp>
2025-09-02 18:50:26 +0900

    .js.mapなどをignoreへ

--- src/TextBox.d.ts
+++
@@ -1,2 +0,0 @@
-export default function TextBox(): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=TextBox.d.ts.map
--- src/components/SearchBox.d.ts
+++
@@ -1,7 +0,0 @@
-type SearchBoxProps = {
-    searchTerm: string;
-    setSearchTerm: (value: string) => void;
-};
-export default function SearchBox({ searchTerm, setSearchTerm }: SearchBoxProps): import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=SearchBox.d.ts.map
